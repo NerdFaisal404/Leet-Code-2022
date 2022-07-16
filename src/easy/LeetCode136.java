de7438a583f24e23db68e56b393e08ed7fe85e1b
faisalahmed <faisal.ahmedbcse@gmail.com>
2022-07-17 02:36:47 +0600

    LeetCode : Solve problem no 412 and 136

---
+++ src/easy/LeetCode136.java
@@ -0,0 +1,11 @@
+package easy;
+
+class LeetCode136 {
+    public int singleNumber(int[] nums) {
+        int ans = nums[0];
+        for(int i =1; i<nums.length; i++){
+            ans = ans^nums[i];
+        }
+        return ans;
+    }
+}
